baee6b65fa3ceb60c8e4223063b298f02a9cb899
Ramesh Meduri <rameshmeduri@outlook.com>
2019-03-07 18:44:02 +0800

    Updates

---
+++ precache-manifest.e030684dc79f0f8ec387616459c97c92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f210d6c8cbf4adc5ff3",
+    "url": "https://rameshmeduri.github.io/test/static/js/runtime~main.d4459696.js"
+  },
+  {
+    "revision": "fb5a92474589c9e809b8",
+    "url": "https://rameshmeduri.github.io/test/static/js/main.e1590c23.chunk.js"
+  },
+  {
+    "revision": "81cc152ed616692cf068",
+    "url": "https://rameshmeduri.github.io/test/static/js/2.ba9ae3cc.chunk.js"
+  },
+  {
+    "revision": "fb5a92474589c9e809b8",
+    "url": "https://rameshmeduri.github.io/test/static/css/main.a4c49e45.chunk.css"
+  },
+  {
+    "revision": "934fb8be9f1647fe1d06bed056448f33",
+    "url": "https://rameshmeduri.github.io/test/index.html"
+  }
+];
